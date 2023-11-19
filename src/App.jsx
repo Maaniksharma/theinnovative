baf039d48b837d627a617096795a96a38a0c8379
maanik sharma <manikss123456@gmail.com>
2023-11-19 16:16:46 +0530

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "./components/Navbar.jsx";
-import Mainpage from "./Mainpage.jsx";
+import MainPage from "./MainPage.jsx";
 import Footer from "./components/Footer.jsx";
-import ContactPage from "./components/ContactPage.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,11 +22,6 @@ const App = () => (
           <Navbar />
         </header>
         <Mainpage />
-        {/* <Routes>
-          <Route path="/projects" element={<ProjectPage />} />
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes> */}
         <Footer />
       </QueryClientProvider>
     </BrowserRouter>
